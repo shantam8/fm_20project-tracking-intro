a3f6890622627dcf6f23e38a83186e49883c0fad
shantam8 <dscharmen@aol.com>
2021-06-20 18:25:07 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,20 @@
 let menuIcon = document.querySelector("#menuIcon");
 let navMenu = document.querySelector("#navMenu");
 
-
-function toggleMobileMenu(){
-
-    if(navMenu.classList.contains("open")){
-        console.log("closing");
-        menuIcon.src = "./images/icon-hamburger.svg";
-        navMenu.classList.remove("open");
-    } else{
-        console.log("opening");
-        menuIcon.src = "./images/icon-close.svg";
-        navMenu.classList.add("open");
-    }
-
-
+function toggleMobileMenu() {
+  if (navMenu.classList.contains("open")) {
+    console.log("closing");
+    menuIcon.src = "./images/icon-hamburger.svg";
+    navMenu.classList.remove("open");
+  } else {
+    console.log("opening");
+    menuIcon.src = "./images/icon-close.svg";
+    navMenu.classList.add("open");
+  }
 }
 
-
-
-function init(){
-    menuIcon.addEventListener("click", toggleMobileMenu);
+function init() {
+  menuIcon.addEventListener("click", toggleMobileMenu);
 }
-
-
 
 window.onload = init();
